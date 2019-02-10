877ce0778ec9bb2f6b9be8806806ef96c5ec8d9f
Shishic <pasukmc@gmail.com>
2019-02-10 14:45:19 +0300

    rework structure, import all nesseasry styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+// import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="site-wrap">
+
+        </div>
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<p>*/}
+            {/*mew mew mew*/}
+          {/*</p>*/}
+        {/*</header>*/}
+        <h1>Заголовок h1</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad culpa dolorem doloremque dolorum fugiat libero magni nesciunt officiis pariatur porro quidem, quisquam, reiciendis repellat rerum sapiente sunt, suscipit. Facere, suscipit.</p>
+        <h2>Заголовок h2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius fugit laborum reprehenderit sed vitae. Accusamus alias aperiam commodi doloremque eligendi, enim, illo impedit, incidunt nemo neque quae sapiente unde veritatis!</p>
+        <h3>Заголовок h3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius fugit laborum reprehenderit sed vitae. Accusamus alias aperiam commodi doloremque eligendi, enim, illo impedit, incidunt nemo neque quae sapiente unde veritatis!</p>
+        <h4>Заголовок h4</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius fugit laborum reprehenderit sed vitae. Accusamus alias aperiam commodi doloremque eligendi, enim, illo impedit, incidunt nemo neque quae sapiente unde veritatis!</p>
+        <h5>Заголовок h5</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius fugit laborum reprehenderit sed vitae. Accusamus alias aperiam commodi doloremque eligendi, enim, illo impedit, incidunt nemo neque quae sapiente unde veritatis!</p>
+        <h6>Заголовок h6</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius fugit laborum reprehenderit sed vitae. Accusamus alias aperiam commodi doloremque eligendi, enim, illo impedit, incidunt nemo neque quae sapiente unde veritatis!</p>
+        <a className='btn btn-primary' href="https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files">dsdsds</a>
       </div>
     );
   }
