46c38d4a33ee66c36cd533cfdc654de09830b2d3
Shishic <pasukmc@gmail.com>
2019-02-10 16:48:44 +0300

    rebuild

---
+++ build/precache-manifest.e1ff306289c99063ce7baebcc9c2956e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad4e08740474893c8f08df87268544ce",
+    "url": "/ecommerce/static/media/Roboto-Italic.ad4e0874.woff2"
+  },
+  {
+    "revision": "daa4b0a47545e1cfb90c",
+    "url": "/ecommerce/static/css/main.d3d925ba.chunk.css"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/ecommerce/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "865764ac87134f6e01dd",
+    "url": "/ecommerce/static/js/runtime~main.865764ac.js"
+  },
+  {
+    "revision": "ed4b08d2702fa26acc324ef1e89ae837",
+    "url": "/ecommerce/static/media/Roboto-Light.ed4b08d2.woff2"
+  },
+  {
+    "revision": "f3a02e2578bee50e620e515912278bc9",
+    "url": "/ecommerce/static/media/Roboto-Bold.f3a02e25.woff2"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/ecommerce/static/media/Roboto-Regular.9feb0110.woff2"
+  },
+  {
+    "revision": "daa4b0a47545e1cfb90c",
+    "url": "/ecommerce/static/js/main.daa4b0a4.chunk.js"
+  },
+  {
+    "revision": "50d01d3e6c994995bcaf829e63d53d1a",
+    "url": "/ecommerce/static/media/Roboto-Medium.50d01d3e.woff2"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/ecommerce/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "10ad0f861c0c5807734017c341940649",
+    "url": "/ecommerce/static/media/Roboto-Light.10ad0f86.woff"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/ecommerce/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "/ecommerce/static/media/Roboto-Medium.9a3bf7ac.woff"
+  },
+  {
+    "revision": "fd5970128586cba8e7ed994f134ec176",
+    "url": "/ecommerce/static/media/Roboto-Italic.fd597012.woff"
+  },
+  {
+    "revision": "372e4f7178759a1d04a50bf4dd74d21f",
+    "url": "/ecommerce/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6ae0a233422723e46b3dcd67114a55f.js"
+  "/ecommerce/precache-manifest.e1ff306289c99063ce7baebcc9c2956e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ecommerce/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
